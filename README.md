07cf0758f49c000e560969a24b3d571eec65f290
shahd1995913 <shahdalkhatib95@gmail.com>
2021-11-16 23:16:26 +0400

    update 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Django CRUD and Forms Overview
+# The Pull Requst : 
--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from django.urls import reverse
+
+from django.contrib.auth import get_user_model
+
+
+
+
+class Snack(models.Model) :
+
+    name = models.CharField(max_length=64)
+    
+   
+    purchaser = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+
+    description = models.TextField(default='')
+    
+    def get_absolute_url(self):
+    
+   
+        return reverse("snack_detail", args=[str(self.id)])
+
+
+    def __str__(self):
+    
+        return self.name
+
